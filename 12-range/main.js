458cc9a3cb20c47d2f2f25aaa07785ec636d07cf
yananlz <1540356239@qq.com>
2021-03-11 17:10:34 +0800

    message

--- 12-range/main.js
+++ 12-range/main.js
@@ -1,3 +1,12 @@
 $(function(){
-  console.log('hello world!');
+    //get elem:range,span
+    //range change event
+    //get range value
+    //set span text
+    let $range=$('input'),
+        $age=$('span');
+                        
+    $range.change(()=>{
+        $age.html($range.val());
+    })
 });
