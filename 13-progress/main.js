ec8a4571a61921e09f4ce4d85cadb1743da10619
张亚楠 <44825312+yananlz@users.noreply.github.com>
2021-03-12 11:38:37 +0800

    Update main.js

--- 13-progress/main.js
+++ 13-progress/main.js
@@ -1,3 +1,37 @@
 $(function(){
-  console.log('hello world!');
+  let $start=$('#start'),       
+      $stop=$('#stop'),        
+      $reset=$('#reset'),
+      $progress=$('progress'),
+      timer=0,
+      arr=new Array();
+
+$start.click(()=>{
+   // timer=timier+1;    
+    myTimer=setInterval(()=>{
+    if($progress.val()<100){  
+      $progress.val($progress.val()+9);
+    }else{
+      clearInterval(myTimer);
+    }
+    
+  },1000)
+  arr.push(myTimer);
+   timer=timer+1;
+
+})
+$stop.click(()=>{
+   
+    for(i=0;i<arr.length;i++){
+      clearInterval(arr[i]);
+    }
+  
+
+})
+$reset.click(()=>{
+  $progress.val(0);  
+  clearInterval(myTimer);
+})
+
 });
+
